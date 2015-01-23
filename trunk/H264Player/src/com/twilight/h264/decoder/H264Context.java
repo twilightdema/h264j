1ab061ee0be78d1819485a92eed9050dfb7b2683
gid15@web.de <gid15@web.de@7ca1b390-e3e5-e677-77fb-91987a93fe79>
2015-01-23 16:54:32 +0000

    Fixed parsing of extradata at codec initialization.

--- trunk/H264Player/src/com/twilight/h264/decoder/H264Context.java
+++ trunk/H264Player/src/com/twilight/h264/decoder/H264Context.java
@@ -8486,7 +8486,7 @@ public class H264Context {
 	        p_offset += 6;
 	        for (i = 0; i < cnt; i++) {
 	            //nalsize = AV_RB16(p) + 2;
-	        	nalsize = ((p_base[p_offset] & 0x0ff) | ((p_base[p_offset+1] & 0x0ff) << 8))+2;
+	        	nalsize = ((p_base[p_offset+1] & 0x0ff) | ((p_base[p_offset] & 0x0ff) << 8))+2;
 	            if(decode_nal_units(p_base, p_offset, nalsize) < 0) {
 	                //av_log(avctx, AV_LOG_ERROR, "Decoding sps %d from avcC failed\n", i);
 	                return -1;
@@ -8497,7 +8497,7 @@ public class H264Context {
 	        cnt = p_base[p_offset++]; // Number of pps
 	        for (i = 0; i < cnt; i++) {
 	            //nalsize = AV_RB16(p) + 2;
-	        	nalsize = ((p_base[p_offset] & 0x0ff) | ((p_base[p_offset+1] & 0x0ff) << 8))+2;
+	        	nalsize = ((p_base[p_offset+1] & 0x0ff) | ((p_base[p_offset] & 0x0ff) << 8))+2;
 	            if(decode_nal_units(p_base, p_offset, nalsize)  != nalsize) {
 	                //av_log(avctx, AV_LOG_ERROR, "Decoding pps %d from avcC failed\n", i);
 	                return -1;
